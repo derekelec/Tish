347bb8910d034a8f54bac109b17b15164f110514
derekelec <mathewson17@live.ca>
2017-02-05 12:40:16 -0800

    package.json setup

---
+++ routes.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var express = require('express');
+var router = express.Router();
+var User = require('./models/user').User;
+
+router.post('/user/register', function(req, res){
+  
+});
